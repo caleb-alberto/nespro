6c085f566a188028e94993f609c2531f11381e33
caleb-alberto <calberto3418@gmail.com>
2024-11-28 22:50:24 -0800

    startServer() implemented

--- tcpServer.cpp
+++ tcpServer.cpp
@@ -1,12 +1,24 @@
 #include "tcpServer.h"
+#include <sys/socket.h>
 
-namespace https {
-    TCPserver::TCPserver() {
+TCPserver::TCPserver(std::string ip, int port) {
+    mSocketAddr.sin_family = AF_INET;
+    mSocketAddr.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &(mSocketAddr.sin_addr));
+
+    if (startServer() != 0) {
+        //error handle
     }
 
-    TCPserver::~TCPserver() {
-    }
+}
 
-    void TCPserver::startListen() {
-    }
+TCPserver::~TCPserver() {
+}
+
+int TCPserver::startServer() {
+    mSocket = socket(AF_INET, SOCK_STREAM, 0);
+    return bind(mSocket, (const struct sockaddr *)&mSocketAddr, sizeof(mSocketAddr));
+}
+
+void TCPserver::startListen() {
 }
--- tcpServer.h
+++ tcpServer.h
@@ -1,10 +1,17 @@
+#include <string>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-namespace https {
-    class TCPserver {
-    public:
-        TCPserver();
-        ~TCPserver();
-        void startListen();
-    };
-}
+class TCPserver {
+public:
+    TCPserver(std::string ip, int port);
+    ~TCPserver();
+    void startListen();
+
+private:
+    int startServer();
+
+    sockaddr_in mSocketAddr;
+    int mSocket;
+};
