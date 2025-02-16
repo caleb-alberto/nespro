0bb5286c12390dd52e49e85a7c87c2c0ed225c26
caleb-alberto <calberto3418@gmail.com>
2025-02-16 14:30:28 -0800

    [add] Implement reverse proxy functionality using curl

--- sample_backend.js
+++ sample_backend.js
@@ -1,23 +1,27 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/backend') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-
-    res.end(`
+    const res_body = `
       <!DOCTYPE html>
       <html lang="en">
       <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Backend Server</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Backend Server</title>
       </head>
       <body>
-        <h1>Hello from the Backend!</h1>
-        <p>This is a sample response from the /backend endpoint.</p>
+	<h1>Hello from the Backend!</h1>
+	<p>This is a sample response from the /backend endpoint.</p>
       </body>
-      </html>
-    `);
+      </html>`;
+    const contentLength = Buffer.byteLength(res_body);
+
+  if (req.url === '/backend') {
+      res.writeHead(200, { 'Content-Type': 'text/html',
+			   'Content-Length': contentLength
+			 });
+
+    res.end(res_body);
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('404 Not Found');
